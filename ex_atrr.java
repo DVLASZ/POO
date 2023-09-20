ba0e09a75fd79b5cbdeafe11178e34f7e954e6a9
DVLASZ <edwarddavilas36@gmail.com>
2023-09-20 09:56:47 -0500

    Estructuración del github

--- ex_atrr.java
+++
@@ -1,9 +0,0 @@
-package call.unicauca.edu.co;
-
-public class ex_atrr {
-
-    public String name = "Edward Dávila";
-    private String document = "don't be a curious";
-    public int year_hdb = 2006;
-    
-}

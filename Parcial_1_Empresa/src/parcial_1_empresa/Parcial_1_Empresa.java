8d4ebce0ff84e855985004d8da31e4697fa9bf1f
DVLASZ <142423275+DVLASZ@users.noreply.github.com>
2023-11-04 20:51:27 -0500

    Update Parcial_1_Empresa.java
    Correccion con los datos de los empleados

--- Parcial_1_Empresa/src/parcial_1_empresa/Parcial_1_Empresa.java
+++ Parcial_1_Empresa/src/parcial_1_empresa/Parcial_1_Empresa.java
@@ -1,93 +1,94 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package parcial.pkg1;
-
 import Empleados.*;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Empleado[] empleados = new Empleado[10];
-        Scanner entrada = new Scanner(System.in);
-        for (int i = 0; i < empleados.length; i++) {
-            if (i % 2 == 0) {
-                System.out.println("Nombre ");
-                String nombre = entrada.next();
-                System.out.println("salarioBase ");
-                double salarioBase = entrada.nextDouble();
-                System.out.println("deducciones ");
-                double deducciones = entrada.nextDouble();
-                empleados[i] = new EmpleadoAsalariado(nombre, salarioBase, deducciones);
-            } else {
-                System.out.println("Nombre ");
-                String nombre = entrada.next();
-                System.out.println("salarioBase ");
-                double salarioBase = entrada.nextDouble();
-                System.out.println("horasTrabajadas ");
-                int horasTrabajadas = entrada.nextInt();
-                System.out.println("tarifaPorHora ");
-                double tarifaPorHora = entrada.nextDouble();
-                empleados[i] = new EmpleadoPorHoras(nombre, salarioBase, horasTrabajadas, tarifaPorHora);
+        Empleado[] arrEmpleados = new Empleado[10];
+        arrEmpleados[0] = new EmpleadoAsalariado("Miguel Sarria", 2300000, 40000);
+        arrEmpleados[1] = new EmpleadoAsalariado("Jherson Castro", 7283036, 89916);
+        arrEmpleados[2] = new EmpleadoAsalariado("Aurelia Duarte", 5797876, 123496);
+        arrEmpleados[3] = new EmpleadoAsalariado("Carlos Valle", 6121420, 75960);
+        arrEmpleados[4] = new EmpleadoAsalariado("Roberto Gomez", 2027462, 142758);
+        arrEmpleados[5] = new EmpleadoAsalariado("Jhon Carmin", 2991048, 118686);
+        arrEmpleados[6] = new EmpleadoPorHoras("Juan Bernal", 27, 5000);
+        arrEmpleados[7] = new EmpleadoPorHoras("Elizabeth Salazar", 36, 5000);
+        arrEmpleados[8] = new EmpleadoPorHoras("Michaela Martinez", 28, 5000);
+        arrEmpleados[9] = new EmpleadoPorHoras("Ivan Martinez", 41, 5000);
+
+        //Imprime todos los empleados, bien bonito 
+        for (Empleado item : arrEmpleados) {
+            System.out.println(item);
+            System.out.println("================================");
+        }
+
+        //Modificar a un objeto del arreglo
+        int opc = 0;
+        do {
+            System.out.println("Modificar el dato de un objeto en el array!");
+
+            int position = getPositionEmpleadoByName(arrEmpleados, Readers.readString("Dime el nombre del empleado"));
+            if (position != -1) {
+                if (arrEmpleados[position].getClass().equals(EmpleadoAsalariado.class)) {
+                    EmpleadoAsalariado refEmpleado = (EmpleadoAsalariado) arrEmpleados[position];
+                    switch (Readers.readInt("1- Modificar el Nombre 2-Modificar el salario base 3-salir")) {
+                        case 1 ->
+                            refEmpleado.setNombre(Readers.readString("Ingresa el nuevo nombre"));
+                        case 2 ->
+                            refEmpleado.setSalarioBase(Readers.readDouble("Ingrese el nuevo salario base"));
+                        case 3 ->
+                            opc = 5;
+                    }
+                } else {
+                    EmpleadoPorHoras refEmpleado = (EmpleadoPorHoras) arrEmpleados[position];
+                    switch (Readers.readInt("1- Modificar el Nombre 2-Modificar la tarifa por horas 3-Modificar las horas trabajadas 4-salir")) {
+                        case 1 ->
+                            refEmpleado.setNombre(Readers.readString("Ingresa el nuevo nombre"));
+                        case 2 ->
+                            refEmpleado.setTarifaPorHora(Readers.readDouble("Cambiemos la tarifa por hora"));
+                        case 3 ->
+                            refEmpleado.setHorasTrabajadas(Readers.readInt("Cuantas horas trabajo, campeon?"));
+                        case 4 ->
+                            opc = 5;
+                    }
+                }
+            }
+        } while (opc != 5);
+
+        System.out.println("");
+
+        //Imprimo de nuevo los empleados para ver si hubo cambios
+        for (Empleado item : arrEmpleados) {
+            System.out.println(item);
+            System.out.println("================================");
+        }
+
+        System.out.println("");
+        //Obtener empleados por horas
+        System.out.println("Obteniendo empleados por horas");
+        for (Empleado item : arrEmpleados) {
+            if (item.getClass().equals(EmpleadoPorHoras.class)) {
+                System.out.println(item.toString());
             }
         }
-        for (Empleado i : empleados) {
-            System.out.println(i);
-        }
-        int posicion = 0;
-        System.out.println("Modificar empleado");
-        posicion = encontrarEmpleadoPorNombre(empleados);
-        if (posicion != -1) {
-            if (empleados[posicion].getClass().equals(EmpleadoPorHoras.class)) {
 
-                EmpleadoPorHoras empleado = (EmpleadoPorHoras) empleados[posicion];
-
-                System.out.println("Cambiar el nombre");
-                empleado.setNombre(entrada.nextLine());
-
-                System.out.println("Dime las horas trabajadas (Entero positivo)");
-                empleado.setHorasTrabajadas(entrada.nextInt());
-
-                System.out.println("Dime las horas TarifaPorHora (Double positivo)");
-                empleado.setTarifaPorHora(entrada.nextDouble());
-            } else {
-                EmpleadoAsalariado empleado = (EmpleadoAsalariado) empleados[posicion];
-                System.out.println("Cambiar el nombre");
-                empleado.setNombre(entrada.nextLine());
-
-                System.out.println("Cambiar salario base");
-                empleado.setSalarioBase(entrada.nextDouble());
-
-                System.out.println("Cambiar deducciones");
-                empleado.setDeducciones(entrada.nextDouble());
-
+        //Obtener un empleado en especifico por el nombre, he imprimo  sus datos
+        int pos;
+        do {
+            pos = getPositionEmpleadoByName(arrEmpleados, Readers.readString("Dime el nombre del empleado"));
+            if (pos != -1) {
+                System.out.println("Datos del empleado en especifico: " + arrEmpleados[pos].toString());
             }
-        }
-
-        for (Empleado i : empleados) {
-            if (i.getClass().equals(EmpleadoPorHoras.class)) {
-                System.out.println(i);
-            }
-        }
-        posicion = encontrarEmpleadoPorNombre(empleados);
-        if (posicion != -1) {
-            System.out.println(empleados[posicion]);
-        }
+        } while (pos != -1);
     }
 
-    private static int encontrarEmpleadoPorNombre(Empleado[] empleados) {
-        System.out.println("Dime el nombre del empleado");
-        String nombre = new Scanner(System.in).nextLine();
-        for (int i = 0; i < empleados.length; i++) {
-
-            if (empleados[i].getNombre().equals(nombre)) {
+    private static int getPositionEmpleadoByName(Empleado[] arrEmpleados, String nombre) {
+        //En este ejemplo todos los empleados tendrán nombres unicos, sino, solo se va a escoger el primer empleado con ese nombre
+        for (int i = 0; i < arrEmpleados.length; i++) {
+            if (arrEmpleados[i].getNombre().equals(nombre)) {
                 return i;
             }
         }
-        System.out.println("No se encontro el empleado con ese nombre");
+        System.out.println("No se encontro empleado con ese nombre");
         return -1;
     }
-
 }
